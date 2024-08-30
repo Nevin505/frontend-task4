f734ae9af5b9493a8b68709e5ec8c2f54a6ef2e4
Nevin <nevinjohn63@gmail.com>
2024-08-30 22:49:51 +0530

    added sideBar

--- app/lib/Constant.ts
+++ app/lib/Constant.ts
@@ -1,6 +1,7 @@
 import {NavigationLink}   from '../../types/Cutsomtypes'
 
 export const navigationLinks:NavigationLink[]=[
+    {navLabel:"Home",href:"/dashboard",icon:"/icons/home.svg",alt:"GST-Icon"},
     {navLabel:"Add Gst",href:"/dashboard/addGst",icon:"/icons/gst.svg",alt:"GST-Icon"},
     {navLabel:"Bank Details",href:"/dashboard/bankDetails",icon:"/icons/bank.svg",alt:"Bank-Icon"},
     {navLabel:"PanCard",href:"/dashboard/panCard",icon:"/icons/panCard.svg",alt:"PanCard-Icon"},
--- components/ui/side-nav/sideNav.tsx
+++ components/ui/side-nav/sideNav.tsx
@@ -1,26 +1,41 @@
+"use client"
 import Link from "next/link"
+import { useState } from "react";
+import Image from "next/image"
 
 import navStyles from './sideNav.module.css'
-import Image from "next/image"
 
 import {navigationLinks}  from '../../../app/lib/Constant';
 
-const SideNav = () => {
-  return (
-    <nav className={navStyles.sideNavContainer}>
-        <div className={navStyles.navLinksContainer}>
-          <div>
-            <Image src="/icons/hamburger.svg" width={20} height={20} alt="HamBurger"></Image>
-          </div>
-            {navigationLinks.map(navigationLink=>{
-                return <Link href={navigationLink.href} key={navigationLink.navLabel} className={navStyles.navLink}> <Image src={navigationLink.icon} alt={navigationLink.alt} width={20} height={20}/><span>{navigationLink.navLabel}</span></Link>
-            }) }
-           
 
-        </div>
-        <div>
-            <button>SignOut</button>
-        </div>
+const SideNav = () => {
+ const[highlightButton,setHighlightedButton]= useState<string>();
+
+const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
+
+
+  const higlightButton=(navLabel:string)=>{
+    setHighlightedButton(navLabel);
+ }
+
+
+ const toggleSidebar = () => {
+     setIsSidebarCollapsed(!isSidebarCollapsed);
+ };
+  return (
+    <nav className={`${navStyles.sideNavContainer} ${isSidebarCollapsed && navStyles.toggleDisplay}`}>
+        <div className={navStyles.navLinksContainer}>
+           <Image  onClick={toggleSidebar} className={navStyles.expandMore} src={!isSidebarCollapsed ? "/icons/expand-left.svg":"/icons/expand-right.svg"} alt="SignOut Button" width={40} height={40}/>
+          
+          <div>
+            {navigationLinks.map((navigationLink)=>{
+                return <Link onClick={()=>higlightButton(navigationLink.navLabel)} href={navigationLink.href} key={navigationLink.navLabel} className={`${navStyles.navLink} ${highlightButton===navigationLink.navLabel?
+                  navStyles.active:undefined}`}> <Image src={navigationLink.icon} alt={navigationLink.alt} width={20} height={20} title={navigationLink.navLabel}/>{!isSidebarCollapsed &&<span>{navigationLink.navLabel}</span>}</Link>
+            }) }       
+          </div>
+          </div>
+
+            <button className={navStyles.signoutButton}><Image  src="/icons/signout.svg" alt="SignOut Button" width={20} height={20} title="Log Out"/>{!isSidebarCollapsed && 'log Out'}</button>
     </nav>
   )
 }
